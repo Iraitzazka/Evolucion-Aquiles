3bbae2a3d4cd16376c72ca3af68109f85061965e
Iraitz <iraitzazka@gmail.com>
2025-06-25 10:32:25 +0200

    meter todo el codigo dentro de haber iniciado sesion

--- mi_app.py
+++ mi_app.py
@@ -21,8 +21,6 @@ with open(CONFIG_FILE) as file:
 # Pre-hashing all plain text passwords once
 config['credentials'] = stauth.Hasher.hash_passwords(config['credentials'])
 
-
-
 # Cargar archivo de configuración
 def load_config():
     with open(CONFIG_FILE) as file:
@@ -45,42 +43,9 @@ def get_authenticator(config):
 config = load_config()
 authenticator = get_authenticator(config)
 
-
-# Obtener el día actual
-hoy = datetime.now().date()
-
-# Título de la app
-st.title("Evolucion Aquiles")
-
-menu = st.sidebar.radio("Opciones", ["Iniciar sesión", "Registrarse"])
-
-if menu == "Iniciar sesión":
-    try:
-        authenticator.login()
-    except Exception as e:
-        st.error(e)
-    if st.session_state.get('authentication_status'):
-        authenticator.logout()
-        st.write(f'Welcome *{st.session_state.get("name")}*')
-        st.title('Some content')
-    elif st.session_state.get('authentication_status') is False:
-        st.error('Username/password is incorrect')
-    elif st.session_state.get('authentication_status') is None:
-        st.warning('Please enter your username and password')
-
-elif menu == "Registrarse":
-    try:
-        email_of_registered_user, \
-        username_of_registered_user, \
-        name_of_registered_user = authenticator.register_user()
-        if email_of_registered_user:
-            st.success('User registered successfully')
-    except Exception as e:
-        st.error(e)
-
-
 ###########################################################################
 
+##################### Conexion a BD #####################
 SUPABASE_URL = "https://lrlruhxjhqcszslbywxr.supabase.co"
 SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxybHJ1aHhqaHFjc3pzbGJ5d3hyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA4MzM1OTUsImV4cCI6MjA2NjQwOTU5NX0.mtjyLTjXwUmxoMVWttfZ2ugd0Zbaw7sQcBBYc2XdpLI"
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
@@ -109,134 +74,172 @@ def eliminar_fila(id_fila):
     else:
         st.success("Fila eliminada correctamente.")
 
-# Inicializa el estado si no está presente
-if "guardar_click" not in st.session_state:
-    st.session_state.guardar_click = False
-if "confirmar_overwrite" not in st.session_state:
-    st.session_state.confirmar_overwrite = False
+###########################################################################
 
-user = supabase.auth.get_user()
+##################### APP #####################
 
-if user:
+# Obtener el día actual
+hoy = datetime.now().date()
 
-    correo = user.user.email
+# Título de la app
+st.title("Evolucion Aquiles")
 
-    df = obtener_datos(correo)
-    df["fecha"] = pd.to_datetime(df["fecha"])
+menu = st.sidebar.radio("Opciones", ["Iniciar sesión", "Registrarse"])
 
-    # Función para manejar el click
-    def guardar_click_callback():
-        st.session_state.guardar_click = True
-
-    # Input: Dolor Mañanero
-    dolor_mañanero_hoy = st.number_input("Introduce dolor mañanero de hoy", step=1.0, format="%.2f")
-
-    # Input: Correr
-    correr_hoy_opcion = st.radio("¿Has corrido hoy?", ["Sí", "No"])
-    correr_hoy = 1 if correr_hoy_opcion == "Sí" else 0
-
-    # Input: Fuerza
-    fuerza_hoy_opcion = st.radio("¿Has hecho ejercicio de fuerza hoy?", ["Sí", "No"])
-    fuerza_hoy = 1 if fuerza_hoy_opcion == "Sí" else 0
-
-    # Input: Saltos
-    if hoy.weekday() == 1: # Si es Martes
-        dolor_DL_str = st.text_input("Introduce dolor DL de hoy (vacío = None)")
-        dolor_DL = float(dolor_DL_str) if dolor_DL_str.strip() else None
-        dolor_SL_izqL_str = st.text_input("Introduce dolor SL izquierda de hoy (vacío = None)")
-        dolor_SL_izq = float(dolor_SL_izqL_str) if dolor_SL_izqL_str.strip() else None
-        dolor_SL_desplazamiento_str = st.text_input("Introduce dolor SL con desplazamiento de hoy (vacío = None)")
-        dolor_SL_desplazamiento = float(dolor_SL_desplazamiento_str) if dolor_SL_desplazamiento_str.strip() else None
-    else:
-        dolor_DL = None
-        dolor_SL_izq = None
-        dolor_SL_desplazamiento = None
-
-    #Boton Para Guargar
-    st.button("Guardar datos de hoy", on_click=guardar_click_callback)
-    if st.session_state.guardar_click:
-        hoy_str = hoy.isoformat()  # string 'YYYY-MM-DD'
-        if (df["fecha"].dt.date == datetime.now().date()).any():
-            st.warning("Ya hay un valor guardado para hoy.")
-            # Mostrar checkbox de confirmación
-            st.session_state.confirmar_overwrite = st.checkbox("¿Deseas sobrescribir los datos de hoy?", value=st.session_state.confirmar_overwrite)
-
-            if st.session_state.confirmar_overwrite:
-                id_fila = df.loc[df["fecha"].dt.date == datetime.now().date(), "id"].values[0]
-                eliminar_fila(id_fila)
-                insertar_datos({'user': supabase.auth.get_user().user.email, 
-                                'fecha':hoy_str, 
-                                'dolor_mañanaero':dolor_mañanero_hoy, 
-                                'dolor_dl':dolor_DL, 
-                                'dolor_sl_izq':dolor_SL_izq, 
-                                'dolor_sl_desplazamiento':dolor_SL_desplazamiento, 
-                                'correr_hoy':correr_hoy, 
-                                'fuerza_hoy':fuerza_hoy})
-                st.success("Valores sobrescritos.")
-                st.session_state.guardar_click = False
-                st.rerun()
-        else:
-            insertar_datos({'user': supabase.auth.get_user().user.email, 
-                                'fecha':hoy_str, 
-                                'dolor_mañanaero':dolor_mañanero_hoy, 
-                                'dolor_dl':dolor_DL, 
-                                'dolor_sl_izq':dolor_SL_izq, 
-                                'dolor_sl_desplazamiento':dolor_SL_desplazamiento, 
-                                'correr_hoy':correr_hoy, 
-                                'fuerza_hoy':fuerza_hoy})
-            st.success("Valores guardados.")
-            st.session_state.guardar_click = False
-            st.rerun()
-
-    df.replace([None, ''], np.nan, inplace=True)
-
-    # Mostrar gráfico
-    if not df.empty:
-        df = df.sort_values("fecha")
-        fig, ax = plt.subplots(figsize=(10, 6))
-
-        ax.plot(df["fecha"], df["dolor_mañanero"], marker='o', linestyle='-', label='Dolor Mañanero')
+if menu == "Iniciar sesión":
+    try:
+        authenticator.login()
+    except Exception as e:
+        st.error(e)
+    if st.session_state.get('authentication_status'):
+        authenticator.logout()
+        st.write(f'Welcome *{st.session_state.get("name")}*')
         
-        df[["dolor_DL", "dolor_SL_izq", "dolor_SL_desplazamiento"]] = df[["dolor_DL", "dolor_SL_izq", "dolor_SL_desplazamiento"]].apply(pd.to_numeric, errors='coerce')
-        df_interpolado = df[["dolor_DL", "dolor_SL_izq", "dolor_SL_desplazamiento"]].interpolate()
-        ax.plot(df["fecha"], df_interpolado["dolor_DL"], marker=None, linestyle='-', color='red', label='Saltos DL')
-        ax.plot(df["fecha"], df_interpolado["dolor_SL_izq"], marker=None, linestyle='-', color='green', label='Saltos SL izq')
-        ax.plot(df["fecha"], df_interpolado["dolor_SL_desplazamiento"], marker=None, linestyle='-', color='yellow', label='Saltos desplazamiento')
+        # Inicializa el estado si no está presente
+        if "guardar_click" not in st.session_state:
+            st.session_state.guardar_click = False
+        if "confirmar_overwrite" not in st.session_state:
+            st.session_state.confirmar_overwrite = False
 
-        ax.axvspan(pd.Timestamp('2025-05-31'), pd.Timestamp('2025-06-08'), color='purple', alpha=0.15, label='Periodo inactivo')
+        user = supabase.auth.get_user()
 
-        ax.axvline(pd.Timestamp('2025-05-15'), color='black', linestyle='--', linewidth=2)
-        ax.text(pd.Timestamp('2025-05-15'), 7, '1. visita Igor', verticalalignment='bottom', horizontalalignment='left', color='black')
+        if user:
 
-        ax.axvline(pd.Timestamp('2025-05-24'), color='black', linestyle='--', linewidth=2)
-        ax.text(pd.Timestamp('2025-05-24'), 7, 'Quemazon al estirar isquio', verticalalignment='bottom', horizontalalignment='left', color='black')
+            correo = user.user.email
 
-        label = True
-        for fecha, tick in zip(df["fecha"], df["dias_correr"]):
-            if tick == 1:
-                if label:
-                    ax.vlines(x=fecha, ymin=0, ymax=0.5, color='orange', linewidth=3, label='correr')
-                    label = False
+            df = obtener_datos(correo)
+            df["fecha"] = pd.to_datetime(df["fecha"])
+
+            # Función para manejar el click
+            def guardar_click_callback():
+                st.session_state.guardar_click = True
+
+            # Input: Dolor Mañanero
+            dolor_mañanero_hoy = st.number_input("Introduce dolor mañanero de hoy", step=1.0, format="%.2f")
+
+            # Input: Correr
+            correr_hoy_opcion = st.radio("¿Has corrido hoy?", ["Sí", "No"])
+            correr_hoy = 1 if correr_hoy_opcion == "Sí" else 0
+
+            # Input: Fuerza
+            fuerza_hoy_opcion = st.radio("¿Has hecho ejercicio de fuerza hoy?", ["Sí", "No"])
+            fuerza_hoy = 1 if fuerza_hoy_opcion == "Sí" else 0
+
+            # Input: Saltos
+            if hoy.weekday() == 1: # Si es Martes
+                dolor_DL_str = st.text_input("Introduce dolor DL de hoy (vacío = None)")
+                dolor_DL = float(dolor_DL_str) if dolor_DL_str.strip() else None
+                dolor_SL_izqL_str = st.text_input("Introduce dolor SL izquierda de hoy (vacío = None)")
+                dolor_SL_izq = float(dolor_SL_izqL_str) if dolor_SL_izqL_str.strip() else None
+                dolor_SL_desplazamiento_str = st.text_input("Introduce dolor SL con desplazamiento de hoy (vacío = None)")
+                dolor_SL_desplazamiento = float(dolor_SL_desplazamiento_str) if dolor_SL_desplazamiento_str.strip() else None
+            else:
+                dolor_DL = None
+                dolor_SL_izq = None
+                dolor_SL_desplazamiento = None
+
+            #Boton Para Guargar
+            st.button("Guardar datos de hoy", on_click=guardar_click_callback)
+            if st.session_state.guardar_click:
+                hoy_str = hoy.isoformat()  # string 'YYYY-MM-DD'
+                if (df["fecha"].dt.date == datetime.now().date()).any():
+                    st.warning("Ya hay un valor guardado para hoy.")
+                    # Mostrar checkbox de confirmación
+                    st.session_state.confirmar_overwrite = st.checkbox("¿Deseas sobrescribir los datos de hoy?", value=st.session_state.confirmar_overwrite)
+
+                    if st.session_state.confirmar_overwrite:
+                        id_fila = df.loc[df["fecha"].dt.date == datetime.now().date(), "id"].values[0]
+                        eliminar_fila(id_fila)
+                        insertar_datos({'user': supabase.auth.get_user().user.email, 
+                                        'fecha':hoy_str, 
+                                        'dolor_mañanaero':dolor_mañanero_hoy, 
+                                        'dolor_dl':dolor_DL, 
+                                        'dolor_sl_izq':dolor_SL_izq, 
+                                        'dolor_sl_desplazamiento':dolor_SL_desplazamiento, 
+                                        'correr_hoy':correr_hoy, 
+                                        'fuerza_hoy':fuerza_hoy})
+                        st.success("Valores sobrescritos.")
+                        st.session_state.guardar_click = False
+                        st.rerun()
                 else:
-                    ax.vlines(x=fecha, ymin=0, ymax=0.5, color='orange', linewidth=3)
+                    insertar_datos({'user': supabase.auth.get_user().user.email, 
+                                        'fecha':hoy_str, 
+                                        'dolor_mañanaero':dolor_mañanero_hoy, 
+                                        'dolor_dl':dolor_DL, 
+                                        'dolor_sl_izq':dolor_SL_izq, 
+                                        'dolor_sl_desplazamiento':dolor_SL_desplazamiento, 
+                                        'correr_hoy':correr_hoy, 
+                                        'fuerza_hoy':fuerza_hoy})
+                    st.success("Valores guardados.")
+                    st.session_state.guardar_click = False
+                    st.rerun()
 
-        label = True
-        for fecha, tick in zip(df["fecha"], df["dias_ejercicio_fuerza"]):
-            if tick == 1:
-                if label:
-                    ax.vlines(x=fecha, ymin=0, ymax=0.5, color='brown', linewidth=1, label='ejercicio fuerza')
-                    label = False
-                else:
-                    ax.vlines(x=fecha, ymin=0, ymax=0.5, color='brown', linewidth=1)
+            df.replace([None, ''], np.nan, inplace=True)
 
-        ax.set_xlabel("Fecha")
-        ax.set_ylabel("Indice Dolor")
-        ax.set_ylim([0, 10.5])
-        ax.grid(True)
-        fig.tight_layout()
-        plt.setp(ax.get_xticklabels(), rotation=45, ha="right")  # rota etiquetas del eje x para mejor lectura
-        ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.25), ncol=3)
-        st.pyplot(fig)
-        plt.close(fig)
-    else:
-        st.info("No hay datos aún.")
+            # Mostrar gráfico
+            if not df.empty:
+                df = df.sort_values("fecha")
+                fig, ax = plt.subplots(figsize=(10, 6))
+
+                ax.plot(df["fecha"], df["dolor_mañanero"], marker='o', linestyle='-', label='Dolor Mañanero')
+                
+                df[["dolor_DL", "dolor_SL_izq", "dolor_SL_desplazamiento"]] = df[["dolor_DL", "dolor_SL_izq", "dolor_SL_desplazamiento"]].apply(pd.to_numeric, errors='coerce')
+                df_interpolado = df[["dolor_DL", "dolor_SL_izq", "dolor_SL_desplazamiento"]].interpolate()
+                ax.plot(df["fecha"], df_interpolado["dolor_DL"], marker=None, linestyle='-', color='red', label='Saltos DL')
+                ax.plot(df["fecha"], df_interpolado["dolor_SL_izq"], marker=None, linestyle='-', color='green', label='Saltos SL izq')
+                ax.plot(df["fecha"], df_interpolado["dolor_SL_desplazamiento"], marker=None, linestyle='-', color='yellow', label='Saltos desplazamiento')
+
+                ax.axvspan(pd.Timestamp('2025-05-31'), pd.Timestamp('2025-06-08'), color='purple', alpha=0.15, label='Periodo inactivo')
+
+                ax.axvline(pd.Timestamp('2025-05-15'), color='black', linestyle='--', linewidth=2)
+                ax.text(pd.Timestamp('2025-05-15'), 7, '1. visita Igor', verticalalignment='bottom', horizontalalignment='left', color='black')
+
+                ax.axvline(pd.Timestamp('2025-05-24'), color='black', linestyle='--', linewidth=2)
+                ax.text(pd.Timestamp('2025-05-24'), 7, 'Quemazon al estirar isquio', verticalalignment='bottom', horizontalalignment='left', color='black')
+
+                label = True
+                for fecha, tick in zip(df["fecha"], df["dias_correr"]):
+                    if tick == 1:
+                        if label:
+                            ax.vlines(x=fecha, ymin=0, ymax=0.5, color='orange', linewidth=3, label='correr')
+                            label = False
+                        else:
+                            ax.vlines(x=fecha, ymin=0, ymax=0.5, color='orange', linewidth=3)
+
+                label = True
+                for fecha, tick in zip(df["fecha"], df["dias_ejercicio_fuerza"]):
+                    if tick == 1:
+                        if label:
+                            ax.vlines(x=fecha, ymin=0, ymax=0.5, color='brown', linewidth=1, label='ejercicio fuerza')
+                            label = False
+                        else:
+                            ax.vlines(x=fecha, ymin=0, ymax=0.5, color='brown', linewidth=1)
+
+                ax.set_xlabel("Fecha")
+                ax.set_ylabel("Indice Dolor")
+                ax.set_ylim([0, 10.5])
+                ax.grid(True)
+                fig.tight_layout()
+                plt.setp(ax.get_xticklabels(), rotation=45, ha="right")  # rota etiquetas del eje x para mejor lectura
+                ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.25), ncol=3)
+                st.pyplot(fig)
+                plt.close(fig)
+            else:
+                st.info("No hay datos aún.")
+
+    elif st.session_state.get('authentication_status') is False:
+        st.error('Username/password is incorrect')
+    elif st.session_state.get('authentication_status') is None:
+        st.warning('Please enter your username and password')
+
+elif menu == "Registrarse":
+    try:
+        email_of_registered_user, \
+        username_of_registered_user, \
+        name_of_registered_user = authenticator.register_user()
+        if email_of_registered_user:
+            st.success('User registered successfully')
+    except Exception as e:
+        st.error(e)
+
+
